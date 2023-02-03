c119db23aa67ef9e360913256a5c5d648929d13d
former <1633852615@qq.com>
2023-02-03 10:50:18 +0800

    封装请求

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+import  request   from './utils/request';
+
+async function getTest(){
+  const res = await request.get('/home/index')
+  console.log(res);
+}
+getTest()
 </script>
 <template>
+  <div>app</div>
 </template>
 <style scoped>
 </style>
