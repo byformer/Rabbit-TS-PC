f652f0bdc7fc5872cda1d6899dbbeedd1ee4a988
former <1633852615@qq.com>
2023-02-05 11:40:28 +0800

    头部吸顶完成

---
+++ src/views/layout/components/app-hander-skitky.vue
@@ -0,0 +1,84 @@
+<script lang="ts" setup name="AppHeaderSticky">
+import { onMounted, ref,onBeforeUnmount } from 'vue';
+import AppHeaderNav from './app-hander-nav.vue'
+
+// 监听滚动条的滚动，获取滚动的距离
+// 监听滚动属于dom事件
+const y = ref(0)
+const onScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+    y.value = scrollTop
+
+}
+onMounted(() => {
+    window.addEventListener('scroll', onScroll)
+})
+// 准备卸载，事件清一下 
+// onBeforeUnmount(() => {
+//     window.removeEventListener('scroll',onScroll)
+// }),
+</script>
+
+<template>
+    <div class="app-header-sticky" :class="{ show: y >= 78 }">
+        <!-- 如果容器没有吸顶出来，那么内容不需要显示 -->
+        <div class="container" v-if=" y >= 78">
+            <RouterLink class="logo" to="/" />
+            <AppHeaderNav />
+            <div class="right">
+                <RouterLink to="/">品牌</RouterLink>
+                <RouterLink to="/">专题</RouterLink>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="less">
+.app-header-sticky {
+    width: 100%;
+    height: 80px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 999;
+    background-color: #fff;
+    border-bottom: 1px solid #e4e4e4;
+    transform: translateY(-100%);
+
+    &.show {
+        transition: all 0.3s linear;
+        transform: translateY(0);
+    }
+
+    .container {
+        display: flex;
+        align-items: center;
+    }
+
+    .logo {
+        width: 200px;
+        height: 80px;
+        background: url(@/assets/images/logo.png) no-repeat right 2px;
+        background-size: 160px auto;
+    }
+
+    .right {
+        width: 220px;
+        display: flex;
+        text-align: center;
+        padding-left: 40px;
+        border-left: 2px solid @xtxColor;
+
+        a {
+            width: 38px;
+            margin-right: 40px;
+            font-size: 16px;
+            line-height: 1;
+
+            &:hover {
+                color: @xtxColor;
+            }
+        }
+    }
+}
+</style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,13 +2,17 @@
 import AppTopNav from './components/app-topnav.vue'
 import AppHander from './components/app-hander.vue'
 import AppFooter from './components/app-footer.vue'
+import AppHanderSkitky from './components/app-hander-skitky.vue';
 </script>
 
 <template>
     <!-- 顶部 -->
     <AppTopNav></AppTopNav>
     <!-- 头部 -->
+    <!-- 默认导航 -->
     <AppHander></AppHander>
+    <!-- 顶部距离超过78px显示 -->
+    <AppHanderSkitky></AppHanderSkitky>
     <!-- 中间 -->
     <main class="app-body">
         <!-- 给路由出口 -->
