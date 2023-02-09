680362deb55d119a2a0f89af016068c9782863e7
former <1633852615@qq.com>
2023-02-09 21:13:24 +0800

    排行榜组件

--- src/views/goods/index.vue
+++ src/views/goods/index.vue
@@ -7,8 +7,8 @@ import GoodsImage from './components/goods-image.vue';
 import GoodsSales from './components/goods-sales.vue'
 import GoodsName from './components/goods-name.vue'
 import GoodsSku from './components/goods-sku.vue'
-import  GoodsDetail  from './components/goods-detail.vue';
-
+import GoodsDetail from './components/goods-detail.vue';
+import GoodsHot from './components/goods-hot.vue'
 const { goods } = useStore()
 const { info } = storeToRefs(goods)
 const route = useRoute()
@@ -17,8 +17,8 @@ watchEffect(() => {
   const id = route.params.id as string
   // 必须id存在，且是商品才发请求
   if (id && route.fullPath === `/goods/${id}`) {
-    
-    
+
+
     goods.resetGoodsInfo()
 
     goods.getGoodsInfo(id)
@@ -65,13 +65,12 @@ const count = ref(1)
               <!-- 商品名称 -->
               <GoodsName :goods="info" />
               <!-- 商品规格 sku组件-->
-              <GoodsSku :goods="info"  
-              @changeSku="changeSku"/>
+              <GoodsSku :goods="info" @changeSku="changeSku" />
               <!-- 数字框组件 -->
               <Numbox v-model:modelValue="count" :min="2" :max="10" />
               <Button type="primary" style="margin-top:20px">加入购物车</Button>
-              
-            </div> 
+
+            </div>
           </div>
           <!-- 商品详情 -->
           <div class="goods-footer">
@@ -83,7 +82,12 @@ const count = ref(1)
               </div>
             </div>
             <!-- 24热榜+专题推荐 -->
-            <div class="goods-aside"></div>
+              <div class="goods-aside">
+                <GoodsHot :type="1" />
+                <GoodsHot :type="2" />
+                <GoodsHot :type="3" />
+           
+            </div>
           </div>
         </div>
 
